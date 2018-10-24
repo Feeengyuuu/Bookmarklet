98e7db012da7768c280fb695777bfaa81cd2edd1
Feeengyuuu <42841438+Feeengyuuu@users.noreply.github.com>
2018-10-23 23:56:43 -0500

    123

---
+++ script.js
@@ -0,0 +1,17 @@
+function no()
+{
+alert("Get Out Then.")
+}
+
+function replace() {
+
+
+    let paragraphs = document.getElementsByTagName( 'body' );
+
+    for ( let i = 0; i < paragraphs.length; i++ ) {
+
+        paragraphs[i].innerHTML = '《红楼梦》 第一回 甄士隐梦幻识通灵　贾雨村风尘怀闺秀此开卷第一回也。作者自云：曾历过一番梦幻之后，故将真事隐去，而借通灵说此《石头记》一书也，故曰“甄士隐”云云。但书中所记何事何人?自己又云：“今风尘碌碌，一事无成，忽念及当日所有之女子：一一细考较去，觉其行止见识皆出我之上。我堂堂须眉诚不若彼裙钗，我实愧则有馀，悔又无益，大无可如何之日也。当此日，欲将已往所赖天恩祖德，锦衣纨绔之时，饫甘餍肥之日，背父兄教育之恩，负师友规训之德，以致今日一技无成、半生潦倒之罪，编述一集，以告天下；知我之负罪固多，然闺阁中历历有人，万不可因我之不肖，自护己短，一并使其泯灭也。所以蓬牖茅椽，绳床瓦灶，并不足妨我襟怀；况那晨风夕月，阶柳庭花，更觉得润人笔墨。我虽不学无文，又何妨用假语村言敷演出来?亦可使闺阁昭传。复可破一时之闷，醒同人之目，不亦宜乎？”故曰“贾雨村”云云。更于篇中间用“梦”“幻”等字，却是此书本旨，兼寓提醒阅者之意。This is the opening section; this the first chapter. Subsequent to the visions of a dream which he had, on some previous occasion, experienced, the writer personally relates, he designedly concealed the true circumstances, and borrowed the attributes of perception and spirituality to relate this story of the Record of the Stone. With this purpose, he made use of such designations as Chen Shih-yin (truth under the garb of fiction) and the like. What are, however, the events recorded in this work? Who are the dramatis personae?';
+
+    }
+
+}
